927b8e693a20c8b9a66227ae96d2b22338c5ca57
Tetiana Proshyna <mollinne2@gmail.com>
2020-12-09 00:16:12 +0200

    Add task 1-5

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,10 @@
+const countProps = function (obj) {
+  const entries = Object.entries(obj);
+  return entries.length;
+};
 
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,33 @@
+const findBestEmployee = function (employees) {
+  const values = Object.values(employees);
+  const bestResult = Math.max(...values);
+  const indexBestResult = values.indexOf(bestResult);
+  const keys = Object.keys(employees);
+  return keys[indexBestResult];
+};
 
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+); // lux
